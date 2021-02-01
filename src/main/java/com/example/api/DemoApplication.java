6a6acab2b955a9d808ad386f217a7eb707390ae4
Ado Pabianko <adopabianko@gmail.com>
2021-02-01 13:57:16 +0700

    default route

--- src/main/java/com/example/api/DemoApplication.java
+++ src/main/java/com/example/api/DemoApplication.java
@@ -2,12 +2,19 @@ package com.example.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String index() {
+		return "Spring Boot API";
 	}
 
 }
